a8b9759da02406763e37147cc95caf82b2831a02
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2023-06-30 23:29:20 +0700

    menetapkan nilai pada ke data field node baru

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -29,6 +29,7 @@ public:
 
 	void insert(string element) //insert a node in the binary search tree
 	{
-		Node* newNode = new Node(element, NULL, NULL); //
+		Node* newNode = new Node(element, NULL, NULL); //Allocate memory for the new node
+		newNode->info = element; 
 	}
 };
