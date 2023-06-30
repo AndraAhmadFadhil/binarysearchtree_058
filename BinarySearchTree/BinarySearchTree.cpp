ddd586c435e40c44689b87143830463118d33aaa
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2023-06-30 23:45:10 +0700

    menambahkan id pada void insert

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -36,6 +36,8 @@ public:
 
 		Node* parent = NULL;
 		Node* currentNode = NULL;
-		search(element, parent, currentNode); 
+		search(element, parent, currentNode); //Locate the node which will be the parent of the node to be inserted
+
+		if (parent == NULL) // If the parent is NULL (tree is empty)
 	}
 };
